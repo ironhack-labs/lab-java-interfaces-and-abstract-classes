9057b7dcffdd7a6dfac96a8479ed95cebf461064
annadpp <anna.depablo.puig@gmail.com>
2024-02-06 19:43:03 +0100

    Car inventory system

---
+++ src/Truck.java
@@ -0,0 +1,13 @@
+public class Truck extends Car {
+    private double towingCapacity;
+
+    public Truck(String vinNumber, String make, String model, int mileage, double towingCapacity) {
+        super(vinNumber, make, model, mileage);
+        this.towingCapacity = towingCapacity;
+    }
+
+    @Override
+    public String getInfo() {
+        return "VIN: " + vinNumber + ", Make: " + make + ", Model: " + model + ", Mileage: " + mileage + ", Towing Capacity: " + towingCapacity;
+    }
+}
