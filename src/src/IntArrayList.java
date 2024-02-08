04578a59afffe0c78eca3cc416ec78f7e023276e
katalinjordans <katalinjordans@gmail.com>
2024-02-08 20:57:28 +0100

    completed all exercises

--- src/src/IntArrayList.java
+++ src/src/IntArrayList.java
@@ -1,15 +1,20 @@
-// IntArrayList should store numbers in an array with a length of 10 by
-// default. When the add method is called, you must first determine if the
-// array is full. If it is, create a new array that is 50% larger, move all
-// elements over to the new array and add the new element. (For example, an
-// array of length 10 would be increased to 15.)
 public class IntArrayList implements IntList {
-    int[] numbers = new int[10];
-    public void add(int number) {
-        // logic
-    };
+    private int capacity = 10;
+    private int[] array = new int[capacity];
 
+    @Override
+    public void add(int number) {
+        if (array.length == capacity) {
+            int newCapacity = array.length + array.length / 2;
+            int[] newArray = new int[newCapacity];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+        array[capacity++] = number;
+    }
+
+    @Override
     public int get(int id) {
-        return numbers[id];
-    };
+        return array[id];
+    }
 }
--- src/src/IntVector.java
+++ src/src/IntVector.java
@@ -1,16 +1,19 @@
-// IntVector should store numbers in an array with a length of 20 by default.
-// When the add method is called, you must first determine if the array is full.
-// If it is, create a new array that is double the size of the current array,
-// move all elements over to the new array and add the new element. (For example,
-// an array of length 10 would be increased to 20.)
 public class IntVector implements IntList {
-    int[] numbers = new int[20];
-
+    private int capacity = 20;
+    private int[] array = new int[capacity];
+    @Override
     public void add(int number) {
-        // logic
-    };
+        if (array.length == capacity) {
+            int newCapacity = array.length + array.length * 2;
+            int[] newArray = new int[newCapacity];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+        array[capacity++] = number;
+    }
 
+    @Override
     public int get(int id) {
-        return numbers[id];
-    };
+        return array[id];
+    }
 }
