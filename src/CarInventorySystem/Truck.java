f1e1f2a805acd8875c99905541b933e3eb733646
Celularfreak <82029406+celularfreak@users.noreply.github.com>
2024-02-08 14:18:41 +0100

    Lab

---
+++ src/CarInventorySystem/Truck.java
@@ -0,0 +1,13 @@
+public class Truck extends Car {
+    private double towingCapacity;
+
+    public Truck(String vinNumber, String make, String model, int mileage, double towingCapacity) {
+        super(vinNumber, make, model, mileage);
+        this.towingCapacity = towingCapacity;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Truck: " + vinNumber + ", " + make + ", " + model + ", Mileage: " + mileage + ", Towing Capacity: " + towingCapacity;
+    }
+}
---
+++ src/IntList/IntArrayList.java
@@ -0,0 +1,27 @@
+public class IntArrayList implements IntList {
+    private int[] array;
+    private int size;
+
+    public IntArrayList() {
+        this.array = new int[10];
+        this.size = 0;
+    }
+
+    @Override
+    public void add(int number) {
+        if (size == array.length) {
+            int[] newArray = new int[array.length + array.length / 2];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+        array[size++] = number;
+    }
+
+    @Override
+    public int get(int id) {
+        if (id < 0 || id >= size) {
+            throw new IndexOutOfBoundsException("Index " + id + " is out of bounds for IntArrayList");
+        }
+        return array[id];
+    }
+}
